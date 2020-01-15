70b13effe0720e38a3992f76cc7dff7b0988587c
eliraudcoel <emmanuellecoel@yahoo.fr>
2020-01-15 14:12:56 +0100

    Rebase master & fix Gemfile and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_133024) do
 ActiveRecord::Schema.define(version: 2020_01_15_121137) do
 
   create_table "administrators", force: :cascade do |t|
@@ -57,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_01_15_121137) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
